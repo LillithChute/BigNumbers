49cdd9fc9c8283f79a55f7535023df8d5c1be041
Chute, Lillith <LChute@memic.com>
2020-10-07 16:27:16 -0400

    Beginning work on the interface for this lab.

---
+++ src/bignumbers/BigNumber.java
@@ -0,0 +1,16 @@
+package bignumbers;
+
+/**
+ * This defines the operations allowed to perform of large numbers stored as sequential lists.
+ */
+public interface BigNumber {
+
+  /**
+   * This returns the number of digits in this number.
+   *
+   * @return The number of digits.
+   */
+  int length();
+
+
+}
